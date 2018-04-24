34c5b901279301fa6331008d8fc3114122eac739
pu1s <pu1s@outlook.com>
2018-04-24 14:06:18 +0400

    +789

--- application/main.cpp
+++ application/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 
+
 #include "../libs/libskyroom/include/libskyroom.h"
 #pragma comment(lib, "libskyroom.lib")
 
@@ -9,9 +10,10 @@
 using namespace skyroom;
 int main()
 {
-	hms __hms;
-	sexagesimal_tag * sg = new sexagesimal(0.999);
-	std::cout << "Hello skyroom!" << sg->get_value() << std::endl;
+	sexagesimal * sg = new sexagesimal();
+	printf("%d", sg->get_value());
+	delete sg;
+	sg = nullptr;
 	system("pause");
 	return 0;
 }

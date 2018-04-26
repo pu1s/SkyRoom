54b64d854f8ae0793dc69e19a8b67254aa667352
Горин Александр <pu1s@outlook.com>
2018-04-26 19:40:28 +0300

    +

--- application/main.cpp
+++ application/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <memory>
 
 
 #include "../libs/libskyroom/include/libskyroom.h"
@@ -10,6 +11,16 @@
 using namespace skyroom;
 int main()
 {
+	sexagesimal* sga;
+	sga = (sexagesimal*)calloc(10, sizeof(sexagesimal));
+	int * countptr = NULL;
+	int count = 15;
+	countptr = &count;
+	sga = (sexagesimal*)realloc(sga, 15 * sizeof(sexagesimal));
+	for (int i = 0; i < 15; i++)
+	{
+		printf("Item number %d: %d\n\r", i, sga[i]);
+	}
 	sexagesimal * sg = new sexagesimal();
 	printf("%d", sg->get_value());
 	delete sg;
